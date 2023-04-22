a4994d88d57348744245864eb726191f454632a5
Olaogunjames <jamezblack19@gmail.com>
2023-04-22 11:19:05 +0100

    iFrame Javascript SDK integration v1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MakePament/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MakePament from './components/MakePament.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MakePament
   }
 }
 </script>
---
+++ src/components/MakePament.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="product">
+    <h2 class="product__name">{{ product.name }}</h2>
+    <p class="product__description">{{ product.description }}</p>
+    <p class="product__price">Price: {{ product.price }} USD</p>
+
+    <div style="display:none;">
+      <label>Card Number:</label>
+      <div id="card-number" class="form-field"></div>
+      <label>CVV:</label>
+      <div id="card-cvv" class="form-field"></div>
+      <!-- comment out or remove the below section if you do not want the customer to enter their billing street and zip for AVS -->
+      <label>Street:</label>
+      <div id="address" class="form-field"></div>
+      <label>Zip:</label>
+      <div id="zipFirst5" class="form-field"></div>
+      <label>Zip+4:</label>
+      <div id="zipPlus4" class="form-field"></div>
+      <input type="hidden" id="avs-fields-data" />
+      <!-- end of AVS section -->
+      <input type="hidden" id="card-token" />
+    </div>
+    <button class="product__button" name="tokenize_and_pay" value="Pay Now" @click="handlePayment">Pay Now</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      product: {
+        name: 'My Product',
+        description: 'This is my favorite product',
+        price: 10.00
+      }
+    }
+  },
+  methods: {
+    async handlePayment() {
+      const ph = await require('../../public/js/ph.js');
+      ph.getToken('Your MID','Your Gateway Public Key');
+    }
+  },
+  mounted() {
+    const ph = require('../../public/js/ph.js');
+    ph.initialize_sdk('3130034155392','526bcd8cb4e1e1f79b211a7b36736c92');
+  },
+}
+</script>
+
+<style scoped>
+.product {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px auto;
+  max-width: 300px;
+}
+
+.product__name {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.product__description {
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+
+.product__price {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.product__button {
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.product__button:hover {
+  background-color: #3e8e41;
+}
+</style>
